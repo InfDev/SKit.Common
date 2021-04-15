16be181b4fc6bc1ed48c7c5ba61721a149dfb535
InfDev <alex.shlyakhto@gmail.com>
2021-04-15 15:13:29 +0300

    Added GetFileHash (md5, sha256) in FileHelper

---
+++ src/Helpers/FileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SKit.Common.Helpers
+{
+    public static class FileHelper
+    {
+        public static string GetFileHashByMD5(string fileName)
+        {
+            using (var hashAlgorithm = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+#pragma warning disable CA1308 // Нормализуйте строки до прописных букв
+                    return BitConverter.ToString(hashAlgorithm.ComputeHash(stream)).Replace("-", string.Empty).ToLowerInvariant();
+#pragma warning restore CA1308 // Нормализуйте строки до прописных букв
+                }
+            }
+        }
+
+        public static string GetFileHashBySHA256(string fileName)
+        {
+            using (var hashAlgorithm = SHA256.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+#pragma warning disable CA1308 // Нормализуйте строки до прописных букв
+                    return BitConverter.ToString(hashAlgorithm.ComputeHash(stream)).Replace("-", string.Empty).ToLowerInvariant();
+#pragma warning restore CA1308 // Нормализуйте строки до прописных букв
+                }
+            }
+        }
+    }
+}
---
+++ test/FileHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Threading;
+using Xunit;
+
+namespace SKit.Common.Tests
+{
+    using SKit.Common.Helpers;
+
+    public class FileHelperTests
+    {
+        private string filePath => @"data\FileForCalcHash.txt";
+        private string hashfilePathForMD5 => filePath + ".md5";
+        private string hashfilePathForSHA256 => filePath + ".sha256";
+
+        private string GetRealHashFromFile(string filePath)
+        {
+            var line = File.ReadAllLines(filePath)[0];
+            var spacePos = line.IndexOf(' ');
+            if (spacePos >= 0)
+                line = line.Substring(0, spacePos);
+            return line;
+        }
+        
+        [Fact]
+        public void MD5()
+        {
+            var calcHash = FileHelper.GetFileHashByMD5(filePath);
+            var realHash = GetRealHashFromFile(hashfilePathForMD5);
+            Assert.Equal(realHash, calcHash);
+        }
+
+        [Fact]
+        public void SHA256()
+        {
+            var calcHash = FileHelper.GetFileHashBySHA256(filePath);
+            var realHash = GetRealHashFromFile(hashfilePathForSHA256);
+            Assert.Equal(realHash, calcHash);
+        }
+    }
+}
